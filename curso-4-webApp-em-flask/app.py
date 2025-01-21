1acd3e289b01c8638e0574c3683472a7f0e7db0d
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-01-21 11:01:02 -0300

    rota pra novo jogo

--- curso-4-webApp-em-flask/app.py
+++ curso-4-webApp-em-flask/app.py
@@ -1,13 +1,29 @@
 from flask import Flask,render_template
 
 
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    lista_jogos= ['fallout 4','skyrim','kenshi']
+    jogo1 = Jogo('Fallout 4', 'sobrevivencia', 'PC')
+    jogo2 = Jogo('Kenshi','RPG','PC')
+    jogo3 = Jogo('Valorant','fps online','PC')
+    
+    lista_jogos= [jogo1,jogo2,jogo3]
     return render_template('Lista.html', titulo='jogos', jogos=lista_jogos)
+
+@app.route('/novo')
+
+def novo():
+    return render_template('novo.html',titulo='jogo')
 
 app.run()
 
