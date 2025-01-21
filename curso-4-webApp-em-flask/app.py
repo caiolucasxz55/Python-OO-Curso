85900f4dea000ed2ecedc7ff56291f3991e1b85d
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-01-21 11:29:01 -0300

    criando e salvando os jogos na lista

--- curso-4-webApp-em-flask/app.py
+++ curso-4-webApp-em-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 
 class Jogo:
@@ -7,17 +7,18 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Fallout 4', 'sobrevivencia', 'PC')
+jogo2 = Jogo('Kenshi','RPG','PC')
+jogo3 = Jogo('Valorant','fps online','PC')
+    
+lista_jogos= [jogo1,jogo2,jogo3]
+
 
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Fallout 4', 'sobrevivencia', 'PC')
-    jogo2 = Jogo('Kenshi','RPG','PC')
-    jogo3 = Jogo('Valorant','fps online','PC')
-    
-    lista_jogos= [jogo1,jogo2,jogo3]
+@app.route('/')
+def index():
     return render_template('Lista.html', titulo='jogos', jogos=lista_jogos)
 
 @app.route('/novo')
@@ -25,8 +26,20 @@ def ola():
 def novo():
     return render_template('novo.html',titulo='jogo')
 
-app.run()
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome,categoria,console)
+    lista_jogos.append(jogo)
+    return render_template('lista.html',titulo='jogos', jogos=lista_jogos)
+
+app.run(debug=True)
 
 
 # # trecho da app
 # app.run(host='0.0.0.0', port=8080)
+
+
