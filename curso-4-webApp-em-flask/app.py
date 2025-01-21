3308f6f15ea18e69126fe85ae4a54ce107012794
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-01-21 13:55:56 -0300

    projeto final

--- curso-4-webApp-em-flask/app.py
+++ curso-4-webApp-em-flask/app.py
@@ -1,59 +1,74 @@
-from flask import Flask,render_template,request,redirect
-
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Jogo:
-    def __init__(self,nome,categoria,console):
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
+class Usuario:
+    def __init__(self, nome, nickname, senha):
         self.nome = nome
-        self.categoria = categoria
-        self.console = console
+        self.nickname = nickname
+        self.senha = senha
 
+usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
+usuario2 = Usuario("Camila Ferreira", "Mila", "paozinho")
+usuario3 = Usuario("Guilherme Louro", "Cake", "python_eh_vida")
 
-
-jogo1 = Jogo('Fallout 4', 'sobrevivencia', 'PC')
-jogo2 = Jogo('Kenshi','RPG','PC')
-jogo3 = Jogo('Valorant','fps online','PC')
-    
-lista_jogos= [jogo1,jogo2,jogo3]
-
+usuarios = { usuario1.nickname : usuario1,
+             usuario2.nickname : usuario2,
+             usuario3.nickname : usuario3 }
 
 app = Flask(__name__)
-
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():
-    return render_template('Lista.html', titulo='jogos', jogos=lista_jogos)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
-
 def novo():
-    return render_template('novo.html',titulo='jogo')
-
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-    jogo = Jogo(nome,categoria,console)
-    lista_jogos.append(jogo)
-    return redirect('/')
-
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if '1234' == request.form['senha']:
-        return redirect('/')
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
-        return redirect('/login')
+        flash('Usuário não logado.')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
-
-
-# # trecho da app
-# app.run(host='0.0.0.0', port=8080)
-
-
